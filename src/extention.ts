d65154f54a9192bcc51c1f3dd1d679454336ceef
Abász <>
2020-05-25 09:05:10 +0200

    Make implementation OS dependent
    Add clang-format npm package to make the formatter OS dependent

--- src/extention.ts
+++ src/extention.ts
@@ -1,13 +1,21 @@
 import { spawn } from "child_process";
 import * as vscode from "vscode";
+import * as path from "path";
+import * as os from "os";
+var clang = require("clang-format");
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.languages.registerDocumentFormattingEditProvider("monkeyc", {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			var child = spawn(
-				`${vscode.extensions.getExtension("ghisguth.monkey-c")!.extensionPath}/LLVM/bin/clang-format.exe`,
-				[document.fileName, "-i", "--style=file", "--fallback-style=google"]
-			);
+			const exePackageLocation = path.dirname(clang.location);
+			var exe;
+			if (os.platform() === "win32") {
+				exe = path.join(exePackageLocation, "/bin/win32/clang-format.exe");
+			} else {
+				exe = path.join(exePackageLocation, `/bin/${os.platform()}_${os.arch()}/clang-format`);
+			}
+
+			var child = spawn(exe, [document.fileName, "-i", "--style=file", "--fallback-style=google"]);
 			return [];
 		}
 	});
